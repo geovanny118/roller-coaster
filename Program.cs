31d7565db3d80c25be3690033677037158271d5c
Geovanni Rojas <geovanny118@hotmail.com>
2021-11-24 23:23:44 -0500

    Se practica metodos de lista

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace EXCERCISE_ARP_5
 {
@@ -6,7 +7,30 @@ namespace EXCERCISE_ARP_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Taller ARP 5");
+            List<int> list_number = new List<int>();
+
+            list_number.Add(1);
+            list_number.Add(2);
+            list_number.Add(3);
+            list_number.Add(4);
+            Console.WriteLine("hay: " + list_number.Count);
+            foreach(var item in list_number){
+                Console.WriteLine("old list: " + item);
+            }
+
+            int x = list_number[0];
+            Console.WriteLine(x);
+            list_number.RemoveAt(0);
+
+            foreach (var item in list_number)
+            {
+                Console.WriteLine("new list: " + item);
+            }
+
+            Console.WriteLine(list_number.Equals(null));
+            list_number.Clear();
+            Console.WriteLine(list_number.Count);
         }
     }
 }
